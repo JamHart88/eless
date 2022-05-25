fee6940a87c2af14f99d2b56503078359e60e423
jamie <hartjp@gmail.com>
2022-05-25 18:09:27 -0400

    Make cleanup script silent

--- test/ifile_test/clean.sh
+++ test/ifile_test/clean.sh
@@ -1,42 +1,43 @@
 #!/usr/bin/sh
 # Cleanup links
-rm brac.hpp
-rm charset.hpp
-rm ch.hpp
-rm cmdbuf.hpp
-rm cmd.hpp
-rm command.hpp
-rm cvt.hpp
-rm debug.hpp
-rm decode.hpp
-rm defines.hpp
-rm edit.hpp
-rm filename.hpp
-rm forwback.hpp
-rm funcs.hpp
-rm ifile.hpp
-rm input.hpp
-rm jump.hpp
-rm less.hpp
-rm lesskey.hpp
-rm lglob.hpp
-rm line.hpp
-rm linenum.hpp
-rm lsystem.hpp
-rm mark.hpp
-rm optfunc.hpp
-rm option.hpp
-rm opttbl.hpp
-rm os.hpp
-rm output.hpp
-rm pattern.hpp
-rm pckeys.hpp
-rm position.hpp
-rm prompt.hpp
-rm regexp.hpp
-rm screen.hpp
-rm search.hpp
-rm signal.hpp
-rm tags.hpp
-rm ttyin.hpp
-rm utils.hpp
+rm -f brac.hpp > /dev/null
+rm -f charset.hpp > /dev/null
+rm -f ch.hpp > /dev/null
+rm -f cmdbuf.hpp > /dev/null
+rm -f cmd.hpp > /dev/null
+rm -f command.hpp > /dev/null
+rm -f cvt.hpp > /dev/null
+rm -f debug.hpp > /dev/null
+rm -f decode.hpp > /dev/null
+rm -f defines.hpp > /dev/null
+rm -f edit.hpp > /dev/null
+rm -f filename.hpp > /dev/null
+rm -f forwback.hpp > /dev/null
+rm -f funcs.hpp > /dev/null
+rm -f ifile.hpp > /dev/null
+rm -f input.hpp > /dev/null
+rm -f jump.hpp > /dev/null
+rm -f less.hpp > /dev/null
+rm -f lesskey.hpp > /dev/null
+rm -f lglob.hpp > /dev/null
+rm -f line.hpp > /dev/null
+rm -f linenum.hpp > /dev/null
+rm -f lsystem.hpp > /dev/null
+rm -f mark.hpp > /dev/null
+rm -f optfunc.hpp > /dev/null
+rm -f option.hpp > /dev/null
+rm -f opttbl.hpp > /dev/null
+rm -f os.hpp > /dev/null
+rm -f output.hpp > /dev/null
+rm -f pattern.hpp > /dev/null
+rm -f pckeys.hpp > /dev/null
+rm -f position.hpp > /dev/null
+rm -f prompt.hpp > /dev/null
+rm -f regexp.hpp > /dev/null
+rm -f screen.hpp > /dev/null
+rm -f search.hpp > /dev/null
+rm -f signal.hpp > /dev/null
+rm -f tags.hpp > /dev/null
+rm -f ttyin.hpp > /dev/null
+rm -f utils.hpp > /dev/null
+rm -f ifile.cpp > /dev/null
